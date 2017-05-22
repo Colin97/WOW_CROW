7b95b8e2291cbd7dbf31eb45fe57d8046a682b29
Colin <l-mh@qq.com>
2017-05-22 22:35:15 +0800

    65536 to 65535

--- src/imu.vhd
+++ src/imu.vhd
@@ -39,7 +39,7 @@ architecture behavioral of imu is
     type state is (st_read_wait, st_read, st_checksum, st_data_out, st_error);
     type buffer_t is array(10 downto 0) of std_logic_vector(7 downto 0);
     constant TIMEOUT_CLOCKS: integer := CLK_FREQ / 1000 * TIMEOUT_MS;
-    constant imu_range: integer := 65536;
+    constant imu_range: integer := 65535;
     constant pos_lower_bound : integer := 24576;
     constant pos_upper_bound : integer := 40960;
     constant speed_lower_bound : integer := 24576;
