ee5ebfac21b77ffae9eb3fe9fec72f08d785c75f
Wende Tan <twd2@163.com>
2017-06-02 20:18:14 +0800

    fix IR

--- src/IR.vhd
+++ src/IR.vhd
@@ -6,45 +6,60 @@ use work.state.all;
 use work.image_info.all;
 use work.signals.all;
 
-entity IR is 
-    generic (
-        lead_low : integer := 9000;
-        lead_high : integer := 4500;
-        data_low : integer := 560;
-        bit_0_high : integer := 560;
-        bit_1_high : integer := 1680;
-        data_length : integer := 32;
-        error_range : integer := 200
-    );
+entity IR is
     port (
         rst : in std_logic;
         clk : in std_logic;
         RX : in std_logic;
         data : out std_logic_vector(31 downto 0);
-        done : out std_logic
+        done : out std_logic;
+        repeat : out std_logic
     );
 end entity IR;
 
 architecture IR_bhv of IR is
-    type state is (s_init, s_lead_low, s_lead_high, s_data_low, s_data_high); 
+    type state is (s_init, s_lead_low, s_lead_high, s_data_low, s_data_high, s_stop_low);
+    constant lead_low : integer := 9000;
+    constant lead_high : integer := 4500;
+    constant repeat_high : integer := 2500;
+    constant data_low : integer := 560;
+    constant bit_0_high : integer := 560;
+    constant bit_1_high : integer := 1680;
+    constant data_length : integer := 32;
+    constant error_range : integer := 200;
+
     signal current_state : state := s_init;
     signal cnt : integer := 0;
     signal current_bit : integer := 0;
-begin 
+    signal rx_buff: std_logic;
+begin
+    -- filter
+    process(clk, rst)
+    begin
+        if rst = '1' then
+            rx_buff <= '1';
+        elsif rising_edge(clk) then
+            rx_buff <= RX;
+        end if;
+    end process;
+
     main : process(clk, rst)
     begin
         if rst = '1' then
             current_state <= s_init;
+            done <= '0';
+            repeat <= '0';
         elsif rising_edge(clk) then
             case current_state is
                 when s_init =>
-                    if RX = '0' then
+                    done <= '0';
+                    repeat <= '0';
+                    if rx_buff = '0' then
                         cnt <= 1;
                         current_state <= s_lead_low;
-                        done <= '0';
                     end if;
                 when s_lead_low => 
-                    if RX = '0' then
+                    if rx_buff = '0' then
                         cnt <= cnt + 1;
                     else 
                         if cnt >= lead_low - error_range and cnt <= lead_low + error_range then
@@ -55,19 +70,22 @@ begin
                         end if;
                     end if;
                 when s_lead_high => 
-                    if RX = '1' then
+                    if rx_buff = '1' then
                         cnt <= cnt + 1;
                     else 
                         if cnt >= lead_high - error_range and cnt <= lead_high + error_range then
                             cnt <= 1;
                             current_state <= s_data_low;
                             current_bit <= 0;
+                        elsif cnt >= repeat_high - error_range and cnt <= repeat_high + error_range then
+                            repeat <= '1';
+                            current_state <= s_stop_low;
                         else
                             current_state <= s_init;
                         end if;
                     end if;
                 when s_data_low => 
-                    if RX = '0' then
+                    if rx_buff = '0' then
                         cnt <= cnt + 1;
                     else 
                         if cnt >= data_low - error_range and cnt <= data_low + error_range then
@@ -78,14 +96,13 @@ begin
                         end if;
                     end if;
                 when s_data_high => 
-                    if RX = '1' then
+                    if rx_buff = '1' then
                         cnt <= cnt + 1;
                     else 
                         if cnt >= bit_0_high - error_range and cnt <= bit_0_high + error_range then
                             data(current_bit) <= '0';
                             if current_bit = 31 then
-                                done <= '1';
-                                current_state <= s_init;
+                                current_state <= s_stop_low;
                             else
                                 current_bit <= current_bit + 1;
                                 current_state <= s_data_low;
@@ -94,15 +111,23 @@ begin
                         elsif cnt >= bit_1_high - error_range and cnt <= bit_1_high + error_range then
                             data(current_bit) <= '1';
                             if current_bit = 31 then
-                                done <= '1';
-                                current_state <= s_init;
+                                current_state <= s_stop_low;
                             else
                                 current_bit <= current_bit + 1;
                                 current_state <= s_data_low;
                                 cnt <= 1;
                             end if;
+                        else
+                            current_state <= s_init;
                         end if;
                     end if;
+                when s_stop_low =>
+                    done <= '1';
+                    if rx_buff = '1' then
+                        current_state <= s_init;
+                    end if;
+                when others =>
+                    current_state <= s_init;
             end case;
         end if;
     end process;
